b4e58ca6084614f379db6b9016d19a1926fd22ca
Ju0011 <yhs07240@naver.com>
2023-01-07 22:18:19 +0900

    state 변경

--- src/App.js
+++ src/App.js
@@ -13,6 +13,14 @@ function App() {
 
   let posts = '강남 고기 맛집';
 
+  function 제목바꾸기() {
+    var newArray = [...글제목]; // deep Copy
+    newArray[0] = '여자 코트 추천';
+    글제목변경(newArray);
+
+    //글제목변경(['여자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
@@ -25,24 +33,26 @@ function App() {
         <hr/>
       </div> */}
 
+      <button onClick={제목바꾸기}>버튼</button>
+
       <div className="list">
-        <h3>{ 글제목[0] } <span onClick={ ()=>{ 좋아요변경(좋아요+1) } }>👍</span> { 좋아요 } </h3>
+        <h3> {글제목[0]} <span onClick={() => { 좋아요변경(좋아요 + 1) }}>👍</span> {좋아요} </h3>
         <p>2월 17일 발행</p>
-        <hr/>
+        <hr />
       </div>
 
       <div className="list">
-        <h3>{ 글제목[1] }</h3>
+        <h3>{글제목[1]} <span onClick={() => { 좋아요변경(좋아요 + 1) }}>👍</span> {좋아요} </h3>
         <p>2월 18일 발행</p>
-        <hr/>
+        <hr />
       </div>
 
       <div className="list">
-        <h3>{ 글제목[2] }</h3>
+        <h3>{글제목[2]}</h3>
         <p>2월 19일 발행</p>
-        <hr/>
+        <hr />
       </div>
-      
+
     </div>
   );
 }
